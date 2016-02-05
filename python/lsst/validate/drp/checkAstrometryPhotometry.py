bacbca3dd3988f0c6f9321203e30b2adad125049
Michael Wood-Vasey <wmwv@pitt.edu>
2016-02-05 01:17:05 -0500

    Remove unused positionDiff function.

--- python/lsst/validate/drp/checkAstrometryPhotometry.py
+++ python/lsst/validate/drp/checkAstrometryPhotometry.py
@@ -104,26 +104,6 @@ def magNormDiff(cat):
     normDiff = (mag - mag_avg) / magerr
 
 
-def positionDiff(cat):
-    """Calculate the diff RA, Dec from the mean for a set of observations an object for each observation.
-
-    @param[in]  cat -- Collection with a .get method
-         for 'coord_ra', 'coord_dec' that returns radians.
-
-    @param[out]  pos_median -- median diff of positions in milliarcsec.  Float.
-    """
-    ra_avg, dec_avg = averageRaDecFromCat(cat)
-    ra, dec = cat.get('coord_ra'), cat.get('coord_dec')
-    # Approximating that the cos(dec) term is roughly the same
-    #   for all observations of this object.
-    ra_diff  = (ra - ra_avg) * np.cos(dec)**2
-    dec_diff = dec - dec_avg
-    pos_diff = np.sqrt(ra_diff**2 + dec_diff**2)  # radians
-    pos_diff = [afwGeom.radToMas(p) for p in pos_diff]  # milliarcsec
-
-    return pos_diff
-
-
 def positionRms(cat):
     """Calculate the RMS for RA, Dec for a set of observations an object.
 
