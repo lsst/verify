provingground-curly/validate_drp

Implement MultiVisitStarBlobSerializer
This serializes matched star catalog statistics along with fits to the
astrometric and photometric error models.

python/lsst/validate/drp/io.py
@@ -361,6 +361,124 @@ class JobSerializer(object):
             self._doc['blobs'].append(m)
 
 
+class MultiVisitStarBlobSerializer(BlobSerializerBase):
+    """Blob for datasets that match stars across multiple visits (with
+    a single filter).
+
+    This serializer is used with AMx, AFx, ADx, PA1, PA2 and PF1 metrics.
+    """
+    def __init__(self):
+        BlobSerializerBase.__init__(self)
+
+    @classmethod
+    def init_from_structs(cls, bandpass, analyze_struct, astrom_struct, phot_struct):
+        """Create a `MultiVisitStarBlobSerializer` from validate_drp's
+        internal data structures.
+
+        Parameters
+        ----------
+        bandpass : `str`
+            Filter name (bandpass).
+        analyze_struct : `lsst.pipebase.Struct`
+            Stellar dataset statistics generated by
+            `lsst.validate.drp.validate.analyzeData`.
+        astrom_struct : `lsst.pipebase.Struct`
+            Dataset produced by `lsst.validate.drp.check.checkAstrometry`.
+        phot_struct : `lsst.pipebase.Struct`
+            Dataset produced by `lsst.validate.drp.check.checkPhotometry`.
+        """
+        args = {}
+
+        # Serialize analyze_struct
+        args['mag'] = DatumSerializer(
+            analyze_struct.mag,
+            'mag',
+            label='{band}'.format(band=bandpass),
+            description='Mean PSF magnitudes of stars over multiple visits')
+        args['mag_rms'] = DatumSerializer(
+            analyze_struct.magrms,
+            'mag',
+            label='RMS({band})'.format(band=bandpass),
+            description='RMS of PSF magnitudes over multiple visits')
+        args['mag_err'] = DatumSerializer(
+            analyze_struct.magerr,
+            'mag',
+            label='sigma({band})'.format(band=bandpass),
+            description='Median 1-sigma uncertainty of PSF magnitudes over '
+                        'multiple visits')
+        args['snr'] = DatumSerializer(
+            analyze_struct.snr,
+            None,
+            label='SNR({band})'.format(band=bandpass),
+            description='Median signal-to-noise ratio of PSF magnitudes over '
+                        'multiple visits')
+        args['dist'] = DatumSerializer(
+            analyze_struct.dist,
+            'milliarcsecond',
+            label='d',
+            description='RMS of sky coordinates of stars over multiple visits')
+        args['safe_flag'] = DatumSerializer(
+            analyze_struct.safeFlag,
+            None,
+            label=None,
+            description='1 where stars is in safe sample, 0 otherwise')
+
+        # serialize astrom_struct
+        args[astrom_struct.model_name] = {}
+        args[astrom_struct.model_name]['doc'] \
+            = "Photometric uncertainty model: mas = C*theta/SNR + sigmaSys"
+        args[astrom_struct.model_name]['C'] = DatumSerializer(
+            astrom_struct.fit_params['C'],
+            None,
+            label='C',
+            description='Scaling factor')
+        args[astrom_struct.model_name]['theta'] = DatumSerializer(
+            astrom_struct.fit_params['theta'],
+            'milliarcsecond',  # TODO replace with 'thetaUnits'
+            label='theta',
+            description='Seeing')  # TODO
+        args[astrom_struct.model_name]['sigmaSys'] = DatumSerializer(
+            astrom_struct.fit_params['sigmaSys'],
+            'milliarcsecond',  # TODO replace with struct's units
+            label='sigma(sys)',
+            description='Systematic error floor')
+        args['astrom_rms'] = DatumSerializer(
+            astrom_struct.astromScatter,
+            'milliarcsecond',  # TODO replace with struct's units
+            label='RMS',
+            description='Astrometric scatter (RMS) for good stars')
+
+        # serialize phot_struct
+        args[phot_struct.model_name] = {}
+        args[phot_struct.model_name]['doc'] \
+            = "Photometric uncertainty model from " \
+              "http://arxiv.org/abs/0805.2366v4 (Eq 4, 5): " \
+              "sigma_1^2 = sigma_sys^2 + sigma_rand^2, " \
+              "sigma_rand^2 = (0.04 - gamma) * x + gamma * x^2 [mag^2] " \
+              "where x = 10**(0.4*(m-m_5))"
+        args[phot_struct.model_name]['sigmaSys'] = DatumSerializer(
+            phot_struct.fit_params['sigmaSys'],
+            'mag',
+            label='sigma(sys)',
+            description='Systematic error floor')
+        args[phot_struct.model_name]['gamma'] = DatumSerializer(
+            phot_struct.fit_params['gamma'],
+            None,
+            label='gamma',
+            description='Proxy for sky brightness and read noise')
+        args[phot_struct.model_name]['m5'] = DatumSerializer(
+            phot_struct.fit_params['m5'],
+            'mag',
+            label='m5',
+            description='5-sigma depth')  # TODO
+
+        return cls(**args)
+
+    @property
+    def schema_id(self):
+        return 'multi-visit-star-blob-v1.0.0'
+
+
 def saveKpmToJson(KpmStruct, filename):
     """Save KPM `lsst.pipe.base.Struct` to JSON file.
 

lsst/verify

python/lsst/validate/drp/io.py
@@ -394,6 +394,125 @@ class JobSerializer(object):
             self._doc['blobs'].append(m)
 
 
+class MultiVisitStarBlobSerializer(BlobSerializerBase):
+    """Blob for datasets that match stars across multiple visits (with
+    a single filter).
+
+    This serializer is used with AMx, AFx, ADx, PA1, PA2 and PF1 metrics.
+    """
+    def __init__(self, **args):
+        BlobSerializerBase.__init__(self)
+        self._doc.update(args)
+
+    @classmethod
+    def init_from_structs(cls, bandpass, analyze_struct, astrom_struct, phot_struct):
+        """Create a `MultiVisitStarBlobSerializer` from validate_drp's
+        internal data structures.
+
+        Parameters
+        ----------
+        bandpass : `str`
+            Filter name (bandpass).
+        analyze_struct : `lsst.pipebase.Struct`
+            Stellar dataset statistics generated by
+            `lsst.validate.drp.validate.analyzeData`.
+        astrom_struct : `lsst.pipebase.Struct`
+            Dataset produced by `lsst.validate.drp.check.checkAstrometry`.
+        phot_struct : `lsst.pipebase.Struct`
+            Dataset produced by `lsst.validate.drp.check.checkPhotometry`.
+        """
+        args = {}
+
+        # Serialize analyze_struct
+        args['mag'] = DatumSerializer(
+            analyze_struct.mag,
+            'mag',
+            label='{band}'.format(band=bandpass),
+            description='Mean PSF magnitudes of stars over multiple visits')
+        args['mag_rms'] = DatumSerializer(
+            analyze_struct.magrms,
+            'mag',
+            label='RMS({band})'.format(band=bandpass),
+            description='RMS of PSF magnitudes over multiple visits')
+        args['mag_err'] = DatumSerializer(
+            analyze_struct.magerr,
+            'mag',
+            label='sigma({band})'.format(band=bandpass),
+            description='Median 1-sigma uncertainty of PSF magnitudes over '
+                        'multiple visits')
+        args['snr'] = DatumSerializer(
+            analyze_struct.snr,
+            None,
+            label='SNR({band})'.format(band=bandpass),
+            description='Median signal-to-noise ratio of PSF magnitudes over '
+                        'multiple visits')
+        args['dist'] = DatumSerializer(
+            analyze_struct.dist,
+            'milliarcsecond',
+            label='d',
+            description='RMS of sky coordinates of stars over multiple visits')
+
+        # serialize astrom_struct
+        args[astrom_struct.model_name] = {}
+        args[astrom_struct.model_name]['doc'] \
+            = "Photometric uncertainty model: mas = C*theta/SNR + sigmaSys"
+        args[astrom_struct.model_name]['C'] = DatumSerializer(
+            astrom_struct.params['C'],
+            None,
+            label='C',
+            description='Scaling factor')
+        args[astrom_struct.model_name]['theta'] = DatumSerializer(
+            astrom_struct.params['theta'],
+            'milliarcsecond',  # TODO replace with 'thetaUnits'
+            label='theta',
+            description='Seeing')  # TODO
+        args[astrom_struct.model_name]['sigmaSys'] = DatumSerializer(
+            astrom_struct.params['sigmaSys'],
+            'milliarcsecond',  # TODO replace with struct's units
+            label='sigma(sys)',
+            description='Systematic error floor')
+        args['astrom_rms'] = DatumSerializer(
+            astrom_struct.astromRmsScatter,
+            'milliarcsecond',  # TODO replace with struct's units
+            label='RMS',
+            description='Astrometric scatter (RMS) for good stars')
+
+        # serialize phot_struct
+        args[phot_struct.model_name] = {}
+        args[phot_struct.model_name]['doc'] \
+            = "Photometric uncertainty model from " \
+              "http://arxiv.org/abs/0805.2366v4 (Eq 4, 5): " \
+              "sigma_1^2 = sigma_sys^2 + sigma_rand^2, " \
+              "sigma_rand^2 = (0.04 - gamma) * x + gamma * x^2 [mag^2] " \
+              "where x = 10**(0.4*(m-m_5))"
+        args[phot_struct.model_name]['sigmaSys'] = DatumSerializer(
+            phot_struct.params['sigmaSys'],
+            'mag',
+            label='sigma(sys)',
+            description='Systematic error floor')
+        args[phot_struct.model_name]['gamma'] = DatumSerializer(
+            phot_struct.params['gamma'],
+            None,
+            label='gamma',
+            description='Proxy for sky brightness and read noise')
+        args[phot_struct.model_name]['m5'] = DatumSerializer(
+            phot_struct.params['m5'],
+            'mag',
+            label='m5',
+            description='5-sigma depth')  # TODO
+        args['phot_rms'] = DatumSerializer(
+            phot_struct.photRmsScatter,
+            'millimag',
+            label='RMS',
+            description='RMS photometric scatter for good stars')
+
+        return cls(**args)
+
+    @property
+    def schema_id(self):
+        return 'multi-visit-star-blob-v1.0.0'
+
+
 def saveKpmToJson(KpmStruct, filename):
     """Save KPM `lsst.pipe.base.Struct` to JSON file.
 