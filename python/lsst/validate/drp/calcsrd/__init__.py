3836f0521832c6e3edd10c5c3e723985fb5bdbf0
Jonathan Sick <jsick@lsst.org>
2016-06-30 20:10:20 +0000

    Add new measurement ADx
    Previously ADx was not measured. This measurement uses the AMx
    measurement as well as the corresponding AFx specification level.

--- python/lsst/validate/drp/calcsrd/__init__.py
+++ python/lsst/validate/drp/calcsrd/__init__.py
@@ -3,3 +3,4 @@ from .pa2 import PA2Measurement  # NOQA
 from .pf1 import PF1Measurement  # NOQA
 from .amx import AMxMeasurement  # NOQA
 from .afx import AFxMeasurement  # NOQA
+from .adx import ADxMeasurement  # NOQA
