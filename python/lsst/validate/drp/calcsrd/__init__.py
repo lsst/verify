d6d42a9741224b80d0e7f2d48ec7efc8419c00e0
Jonathan Sick <jsick@lsst.org>
2016-06-30 19:06:30 +0000

    Add AFxMeasurement
    Note this uses an improved computation over the original calcSrd
    implementation since the actual AMx measurement is used instead of the
    specification level. This matches the language of LPM-17 better.

--- python/lsst/validate/drp/calcsrd/__init__.py
+++ python/lsst/validate/drp/calcsrd/__init__.py
@@ -2,3 +2,4 @@ from .pa1 import PA1Measurement  # NOQA
 from .pa2 import PA2Measurement  # NOQA
 from .pf1 import PF1Measurement  # NOQA
 from .amx import AMxMeasurement  # NOQA
+from .afx import AFxMeasurement  # NOQA
