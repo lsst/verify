c7c6e6bb7a2be465440c183390ba04ff0b82e68b
Jonathan Sick <jsick@lsst.org>
2016-09-20 15:34:05 -0700

    Update __init__ patterns so lsstimport is optional
    - __init__ files for shared namespaces (lsst and lsst.validate) do a
      full pkgutils namespace extension *and* lsstimport; though lsstimport
      is optional for validate_base since it doesn't contain C++ code.
    - __init__ for lsst.validate.base does not use pkgutils or lsstimport
      since it is not a shared namepspace.

--- python/lsst/__init__.py
+++ python/lsst/__init__.py
@@ -1,2 +1,10 @@
-import pkgutil, lsstimport
+# See COPYRIGHT file at the top of the source tree.
+
+try:
+    # validate_base does not formally use on lsstimport, but we still attempt
+    # to import it for compatiblity with the lsst Stack.
+    import lsstimport  # noqa: F401
+except ImportError:
+    pass
+import pkgutil
 __path__ = pkgutil.extend_path(__path__, __name__)
--- python/lsst/validate/base/__init__.py
+++ python/lsst/validate/base/__init__.py
@@ -1,1 +1,6 @@
+# See COPYRIGHT file at the top of the source tree.
+"""A framework for measuring and defining performance metrics that can be
+submitted to the SQUASH service.
+"""
+
 from .base import *  # noqa: F403
