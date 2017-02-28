f117e9e6517172a4654d0569ee2876224f48d8f6
John K. Parejko <parejkoj@uw.edu>
2017-02-28 13:19:18 -0800

    output_measurements, with partial test

---
+++ tests/test_output_measurements.py
@@ -0,0 +1,37 @@
+# See COPYRIGHT file at the top of the source tree.
+from __future__ import absolute_import, division, print_function
+
+import os
+import json
+import unittest
+import astropy.units as u
+
+from lsst.validate.base import Metric, MetricSet
+import lsst.validate.base
+
+
+def read_log(filename):
+    with open(filename) as logfile:
+        result = json.load(logfile)
+    return result
+
+
+class OutputTestCase(unittest.TestCase):
+    def setUp(self):
+        package = 'testing'
+        metric1 = Metric("{}.thing1".format(package), 'thing1', u.arcsec)
+        metric2 = Metric("{}.thing2".format(package), 'thing2', u.mag)
+        self.metric_set = MetricSet("testing", [metric1, metric2])
+
+    # def tearDown(self):
+    #     if os.path.exists(self.outfile):
+    #         os.remove(self.outfile)
+
+    def test_output_measurements_example(self):
+        data = {'thing1': 10, 'thing2': 5.2}
+        self.outfile = lsst.validate.base.output_measurements(self._testMethodName, data)
+        result = read_log(self.outfile)
+        self.assertEqual(data, result)
+
+if __name__ == "__main__":
+    unittest.main()
