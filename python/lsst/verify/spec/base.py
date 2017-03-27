4ac660cccd7ec5ac74b14f62b8c87880d7ce20a3
Jonathan Sick <jsick@lsst.org>
2017-03-27 12:24:49 -0700

    Refactor Specification into ThresholdSpecification
    This is a wide-ranging commit that effects the Specification data model
    described in https://sqr-017.lsst.io
    
    The main feature is that specification is an abstract base class now
    that defines an interface for types of specifications. The one that
    we've already been using is a threshold-like specifications, so that
    functionality is refactored out into a ThresholdSpecification class.
    
    Specifications have types (e.g., `threshold`), and also have a check()
    method that tests a measurement against a specification.
    
    When serializing, all specifications have 'name' and 'type' JSON fields.
    Information particular to the type of specification is embedded in a
    field named after the type itself. Specification subclasses provide this
    value by implementing a _serialize_type method.
    
    Other upgrades:
    
    - __eq__ and __repr__ are implemented in ThresholdSpecification
    - improved argument checking in ThresholdSpecification, raising
      TypeError otherwise.
    - Use the Name class instead of a string for the specification's name.

---
+++ python/lsst/verify/spec/base.py
@@ -0,0 +1,86 @@
+# See COPYRIGHT file at the top of the source tree.
+from __future__ import print_function, division
+
+import abc
+from future.utils import with_metaclass
+
+from ..jsonmixin import JsonSerializationMixin
+from ..naming import Name
+
+__all__ = ["Specification"]
+
+
+class Specification(with_metaclass(abc.ABCMeta, JsonSerializationMixin)):
+    """Specification base class.
+
+    Specification classes must implement:
+
+    - `type`
+    - `serialize_type`
+    - `check`
+
+    Subclasses should also call ``Specification.__init__`` to initialize
+    the specifications ``name`` attribute (a `~lsst.verify.Name`
+    instance).
+    """
+
+    def __init__(self, name):
+        # name attibute must be a Name instance representing a specification
+        if not isinstance(name, Name):
+            self._name = Name(spec=name)
+        else:
+            self._name = name
+        if not self._name.is_spec:
+            message = 'name {0!r} does not represent a specification'
+            raise TypeError(message.format(self._name))
+
+    @property
+    def name(self):
+        """Specification name (`lsst.verify.Name`)."""
+        return self._name
+
+    @abc.abstractproperty
+    def type(self):
+        """Specification type (`str`)."""
+        pass
+
+    @abc.abstractmethod
+    def _serialize_type(self):
+        """Serialize type-specific specification data to a JSON-serializable
+        `dict`.
+
+        This method is used by the `json` property as the value associated
+        with the key named for `type`.
+        """
+        pass
+
+    @property
+    def json(self):
+        """`dict` that can be serialized as semantic JSON, compatible with
+        the SQUASH metric service.
+        """
+        return JsonSerializationMixin.jsonify_dict(
+            {
+                'name': str(self.name),
+                'type': self.type,
+                self.type: self._serialize_type()
+            }
+        )
+
+    @abc.abstractmethod
+    def check(self, measurement):
+        """Check if a measurement passes this specification.
+
+        Parameters
+        ----------
+        measurement : `astropy.units.Quantity`
+            The measurement value. The measurement `~astropy.units.Quantity`
+            must have units *compatible* with `threshold`.
+
+        Returns
+        -------
+        passed : `bool`
+            `True` if the measurement meets the specification,
+            `False` otherwise.
+        """
+        pass
