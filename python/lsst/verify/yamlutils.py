0c219b69c75dc3ad6e673ee671c94bfb2f224314
Jonathan Sick <jsick@lsst.org>
2017-03-29 12:17:01 -0700

    Add load_all_ordered_yaml
    This support the yaml.load_all() function, but providing individual
    documents as OrderedDicts.
    
    This is implemented largely by refactoring the OrderedLoader factory
    from load_ordered_yaml.

--- python/lsst/verify/yamlutils.py
+++ python/lsst/verify/yamlutils.py
@@ -6,11 +6,10 @@ from __future__ import print_function, division
 from collections import OrderedDict
 import yaml
 
-__all__ = ['load_ordered_yaml']
+__all__ = ['load_ordered_yaml', 'load_all_ordered_yaml']
 
 
-def load_ordered_yaml(stream, Loader=yaml.Loader,
-                      object_pairs_hook=OrderedDict):
+def load_ordered_yaml(stream, **kwargs):
     """Load a YAML document from a stream as an `~collections.OrderedDict`.
 
     Parameters
@@ -29,11 +28,39 @@ def load_ordered_yaml(stream, Loader=yaml.Loader,
     yaml_doc : `~collections.OrderedDict`
         The YAML document as an `~collections.OrderedDict` (or the type
         specified in ``object_pairs_hook``).
-
-    Notes
-    -----
-    Solution from http://stackoverflow.com/a/21912744
     """
+    OrderedLoader = _build_ordered_loader(**kwargs)
+    return yaml.load(stream, OrderedLoader)
+
+
+def load_all_ordered_yaml(stream, **kwargs):
+    """Load all YAML documents from a stream as a `list` of
+    `~collections.OrderedDict`\ s.
+
+    Parameters
+    ----------
+    stream :
+        A stream for a YAML file (made with `open` or `~io.StringIO`, for
+        example).
+    loader : optional
+        A YAML loader class. Default is ``yaml.Loader``.
+    object_pairs_hook : obj, optional
+        Class that YAML key-value pairs are loaded into by the ``loader``.
+        Default is `collections.OrderedDict`.
+
+    Returns
+    -------
+    yaml_docs : `list` of `~collections.OrderedDict`
+        The YAML documents as a `list` of `~collections.OrderedDict`\ s (or
+        the type specified in ``object_pairs_hook``).
+    """
+    OrderedLoader = _build_ordered_loader(**kwargs)
+    return yaml.load_all(stream, OrderedLoader)
+
+
+def _build_ordered_loader(Loader=yaml.Loader, object_pairs_hook=OrderedDict):
+    # Solution from http://stackoverflow.com/a/21912744
+
     class OrderedLoader(Loader):
         pass
 
@@ -45,4 +72,4 @@ def load_ordered_yaml(stream, Loader=yaml.Loader,
         yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG,
         construct_mapping)
 
-    return yaml.load(stream, OrderedLoader)
+    return OrderedLoader
