fcdfd3a9a2c7600ffec02ec665a7b3cff1021dbc
Jonathan Sick <jsick@lsst.org>
2017-04-12 17:36:20 -0700

    Migrate Measurement class into lsst.verify design
    This broadly updates the Measurement class to fulfill the new design
    principles of the lsst.verify framework.
    
    - Measurement.metric is now a Metric instance (optional, if available)
    
    - Measurement.metric_name is a Name instance reflecting the name of the
      associated metric.
    
    - The measurement itself is now a `quantity` attribute, and we ensure
      that it is always an astropy.units.Quantity.
    
    - Removed `parameters` for now; these will be replaced with provenance.
    
    - Measurement.extras is implemented as a blob
    
      extras is a blob automatically created for a measurement. It is named
      for the metric, and is automatically serialized with other blobs.
    
    - Add Measurement.link_blob method for other blobs
    
    - Using astropy's function for testing approximate equality of Quantity
      instances (astropy.tests.helper.quantity_allclose).
    
    - Many changes to json serialization:
    
      - updated schema in general. For instance, we serialize blob_refs
        instead of the actual blobs. This is done so that data isn't
        repeated in a JSON serialization; it'll be the responsibility of the
        Job class to serialize blobs alongside measurements.
    
      - Attempt to convert the units of the quantity to those of the
        associated metric's definition, if available.
    
    - Add `deserialize` classmethod for consistency with other APIs; we're
      moving away from teh `from_json` method name. Note that this method
      takes an additional kwarg that is a list of blobs; the actor that's
      deserializing a Measurement will also be deserializing blobs, and can
      provide those now.
    
    - Implement Measurement.extras deserialization
    
      A blob named after the metric is interpreted as being the 'extras' blob
      for that measurement. Thus extras can now be serialized and
      deserialized, entirely using the blob infrastructure.

--- python/lsst/verify/measurement.py
+++ python/lsst/verify/measurement.py
@@ -1,16 +1,18 @@
 # See COPYRIGHT file at the top of the source tree.
 from __future__ import print_function, division
 
+__all__ = ['MeasurementSet', 'Measurement']
+
 import uuid
 
 import astropy.units as u
-from .jsonmixin import JsonSerializationMixin
+from astropy.tests.helper import quantity_allclose
+
 from .blob import Blob
-from .datum import Datum, QuantityAttributeMixin
+from .datum import Datum
+from .jsonmixin import JsonSerializationMixin
 from .metric import Metric
-
-
-__all__ = ['MeasurementSet', 'Measurement']
+from .naming import Name
 
 
 class MeasurementSet(object):
@@ -60,66 +62,143 @@ class MeasurementSet(object):
         return "{0.name}: {{\n{1}\n}}".format(self, items)
 
 
-class Measurement(QuantityAttributeMixin, JsonSerializationMixin):
-    """A realization of a single Metric.
+class Measurement(JsonSerializationMixin):
+    """A measurement of a single Metric.
+
+    Parameters
+    ----------
+    metric : `str`, `lsst.verify.Name`, or `lsst.verify.Metric`
+        The name of this metric or the corresponding `~lsst.verify.Metric`
+        instance. If a `~lsst.verify.Metric` is providing then the units of
+        the ``quantity`` are automatically validated.
+    quantity : `astropy.units.Quantity`, optional
+        The measured value as an Astropy `~astropy.units.Quantity`.
+        If a ``~lsst.verify.Metric`` instance is provided, the units of
+        ``quantity`` are compared to the ``~lsst.verify.Metric`\ 's units
+        for compatibility. The ``quantity`` can also be set, updated, or
+        read from a Measurement instance with the `quantity` property.
+    blobs : `list` of `~lsst.verify.Blob`\ s, optional
+        List of `lsst.verify.Blob` instances that are associated with a
+        measurement. Blobs are datasets that can be associated with many
+        measurements and provide context to a measurement.
+    extras : `dict` of `lsst.verify.Datum` instances, optional
+        `~lsst.verify.Datum` instances can be attached to a measurement.
+        Extras can be accessed from the `Measurement.extras` attribute.
+
+    Raises
+    ------
+    TypeError
+        Raised if arguments are not valid types.
     """
 
-    _metric = None
-    """`Metric` that this is a measurement of."""
-
-    value = None
-    """`astropy.Quantity` the value that was measured.
-    Must be an equivalent unit (i.e. convertible to) of the parent `Metric`.
-    """
-
-    parameters = None
-    """`dict` containing all input parameters used by this measurement.
-    Parameters are `Datum` instances. Parameter values can be accessed
-    and updated as instance attributes named after the parameter.
+    blobs = None
+    """`dict` of `lsst.verify.Blob` instances associated with this measurement.
     """
 
     extras = None
-    """`dict` containing all measurement by-products (called *extras*) that
-    have been registered for serialization.
-
-    Extras are `Datum` instances. Values of extras can also be accessed and
-    updated as instance attributes named after the extra.
+    """`~lsst.verify.Blob` of `lsst.verify.Datum` instances associated with
+    this measurement.
     """
-    def __init__(self, name, value, metric_set=None):
-        """Summary
 
-        Parameters
-        ----------
-        name : 'str'
-            The name of this metric.
-        value : 'astropy.Quantity'
-            The value that was measured for this metric.
-        metric_set : None
-            A `MetricSet` to extract the metric definitions from.
-
-        Raises
-        ------
-        KeyError
-            Raised if name is not a valid name of a metric in metric_set
-        UnitTypeError
-            Raised if value is not convertible to the type of this
-            measurement's metric.
-        """
-        if metric_set is not None:
-            self._metric = metric_set[name]
-        else:
-            raise NotImplementedError('Cannot autoload verify_metrics yet')
-
-        if not self._metric.check_unit(value):
-            raise u.UnitTypeError
-        else:
-            self.value = value
+    def __init__(self, metric, quantity=None, blobs=None, extras=None):
+        # Internal attributes
+        self._quantity = None
+        # every instance gets a unique identifier, useful for serialization
         self._id = uuid.uuid4().hex
 
+        try:
+            self.metric = metric
+        except TypeError:
+            # must be a name
+            self._metric = None
+            self.metric_name = metric
+
+        self.quantity = quantity
+
+        self.blobs = {}
+        if blobs is not None:
+            for blob in blobs:
+                if not isinstance(blob, Blob):
+                    message = 'Blob {0} is not a Blob-type'
+                    raise TypeError(message.format(blob))
+                self.blobs[blob.name] = blob
+
+        # extras is a blob automatically created for a measurement.
+        # by attaching extras to the self.blobs we ensure it is serialized
+        # with other blobs.
+        if str(self.metric_name) not in self.blobs:
+            self.extras = Blob(str(self.metric_name))
+            self.blobs[str(self.metric_name)] = self.extras
+        else:
+            # pre-existing Blobs; such as from a deserialization
+            self.extras = self.blobs[str(self.metric_name)]
+        if extras is not None:
+            for key, extra in extras.items():
+                if not isinstance(extra, Datum):
+                    message = 'Extra {0} is not a Datum-type'
+                    raise TypeError(message.format(extra))
+                self.extras[key] = extra
+
     @property
-    def blobs(self):
-        """`dict` of blobs attached to this measurement instance."""
-        return self._linked_blobs
+    def metric(self):
+        """Metric associated with the measurement (`lsst.verify.Metric` or
+        `None`).
+        """
+        return self._metric
+
+    @metric.setter
+    def metric(self, value):
+        if not isinstance(value, Metric):
+            message = '{0} must be an lsst.verify.Metric-type'
+            raise TypeError(message.format(value))
+
+        # Ensure the existing quantity has compatible units
+        if self.quantity is not None:
+            if not value.check_unit(self.quantity):
+                message = ('Cannot assign metric {0} with units incompatible '
+                           'with existing quantity {1}')
+                raise TypeError(message.format(value, self.quantity))
+
+        self._metric = value
+
+        # Reset metric_name for consistency
+        self.metric_name = value.name
+
+    @property
+    def metric_name(self):
+        """Name of the corresponding metric (`lsst.verify.Name`)."""
+        return self._metric_name
+
+    @metric_name.setter
+    def metric_name(self, value):
+        if not isinstance(value, Name):
+            self._metric_name = Name(metric=value)
+        else:
+            if not value.is_metric:
+                message = "Expected {0} to be a metric's name".format(value)
+                raise TypeError(message)
+            else:
+                self._metric_name = value
+
+    @property
+    def quantity(self):
+        return self._quantity
+
+    @quantity.setter
+    def quantity(self, q):
+        # a quantity can be None or a Quantity
+        if not isinstance(q, u.Quantity) and q is not None:
+            message = '{0} is not an astropy.units.Quantity-type'
+            raise TypeError(message.format(q))
+
+        if self.metric is not None and q is not None:
+            # check unit consistency
+            if not self.metric.check_unit(q):
+                message = ("The quantity's units {0} are incompatible with "
+                           "the metric's units {1}")
+                raise TypeError(message.format(q.unit, self.metric.unit))
+
+        self._quantity = q
 
     @property
     def identifier(self):
@@ -127,42 +206,95 @@ class Measurement(QuantityAttributeMixin, JsonSerializationMixin):
         return self._id
 
     def __str__(self):
-        return "{0.name}: {0.value}".format(self)
-
-    @property
-    def name(self):
-        """Name of the `Metric` associated with this measurement (`str`)."""
-        return self._metric.name
+        return "{self.metric_name!s}: {self.quantity!s}".format(self=self)
 
     @property
     def description(self):
-        return self._metric.description
+        """Description of the metric (`str`, or `None` if
+        `Measurement.metric` is not set).
+        """
+        if self._metric is not None:
+            return self._metric.description
+        else:
+            return None
 
-    # @property
-    # def datum(self):
-    #     """Representation of this measurement as a `Datum`."""
-    #     return Datum(self.quantity, label=self.name,
-    #                  description=self.metric.description)
+    @property
+    def datum(self):
+        """Representation of this measurement as a `Datum`."""
+        return Datum(self.quantity,
+                     label=str(self.metric_name),
+                     description=self.description)
+
+    def link_blob(self, blob):
+        """Link a blob to this measurement.
+
+        Blobs can be linked to a measurement so that they can be retrieved
+        by analysis and visualization tools post-serialization. Blob data
+        is not copied, and one blob can be linked to multiple measurements.
+
+        Parameters
+        ----------
+        blob : `lsst.verify.Blob`
+            A `~lsst.verify.Blob` instance.
+
+        Notes
+        -----
+        After linking, the `Blob` instance can be accessed by name
+        (`Blob.name`) through the `Measurement.blobs` `dict`.
+        """
+        if not isinstance(blob, Blob):
+            message = 'Blob {0} is not a Blob-type'.format(blob)
+            raise TypeError(message)
+        self.blobs[blob.name] = blob
 
     @property
     def json(self):
         """A `dict` that can be serialized as semantic SQUASH JSON."""
-        if isinstance(self.quantity, u.Quantity):
-            _value = self.quantity.value
+        if self.quantity is None:
+            _normalized_value = None
+            _normalized_unit_str = None
+        elif self.metric is not None:
+            # ensure metrics are normalized to metric definition's units
+            _normalized_value = self.quantity.to(self.metric.unit).value
+            _normalized_unit_str = self.metric.unit_str
         else:
-            _value = self.quantity
-        blob_ids = {k: b.identifier for k, b in self._linked_blobs.items()}
-        object_doc = {'metric': self.metric,
+            _normalized_value = self.quantity.value
+            _normalized_unit_str = str(self.quantity.unit)
+
+        blob_refs = [b.identifier for k, b in self.blobs.items()]
+        # Remove any reference to an empty extras blob
+        if len(self.extras) == 0:
+            blob_refs.remove(self.extras.identifier)
+
+        object_doc = {'metric': str(self.metric_name),
                       'identifier': self.identifier,
-                      'value': _value,
-                      'unit': self.unit_str,
-                      'parameters': self.parameters,
-                      'extras': self.extras,
-                      'blobs': blob_ids,
-                      'spec_name': self.spec_name,
-                      'filter_name': self.filter_name}
+                      'value': _normalized_value,
+                      'unit': _normalized_unit_str,
+                      'blob_refs': blob_refs}
         json_doc = JsonSerializationMixin.jsonify_dict(object_doc)
         return json_doc
+
+    @classmethod
+    def deserialize(cls, metric=None, identifier=None, value=None, unit=None,
+                    blob_refs=None, blobs=None, **kwargs):
+        """Create a Measurement instance from a parsed YAML/JSON document.
+        """
+        # Resolve blobs from references:
+        if blob_refs is not None and blobs is not None:
+            # get only referenced blobs
+            _blobs = [blob for blob in blobs if blob.identifier in blob_refs]
+        elif blobs is not None:
+            # use all the blobs if none were specifically referenced
+            _blobs = blobs
+        else:
+            _blobs = None
+
+        # Resolve quantity
+        _quantity = u.Quantity(value, u.Unit(unit))
+
+        instance = cls(metric, quantity=_quantity, blobs=_blobs)
+        instance._identifer = identifier  # re-wire id from serialization
+        return instance
 
     @classmethod
     def from_json(cls, json_data, blobs_json=None):
@@ -181,55 +313,19 @@ class Measurement(QuantityAttributeMixin, JsonSerializationMixin):
         measurement : `MeasurementBase`-type
             Measurement from JSON.
         """
-        q = cls._rebuild_quantity(json_data['value'], json_data['unit'])
-
-        parameters = {k: Datum.from_json(v)
-                      for k, v in json_data['parameters'].items()}
-        extras = {k: Datum.from_json(v)
-                  for k, v in json_data['extras'].items()}
-
-        linked_blobs = {}
+        blobs = []
         if blobs_json is not None:
-            for k, id_ in json_data['blobs'].items():
+            for id_ in json_data['blob_refs']:
                 for blob_doc in blobs_json:
                     if blob_doc['identifier'] == id_:
                         blob = Blob.from_json(blob_doc)
-                        linked_blobs[k] = blob
+                        blobs.append(blob)
 
-        m = cls(quantity=q,
-                id_=json_data['identifier'],
-                metric=Metric.from_json(json_data['metric']),
-                parameters=parameters,
-                linked_blobs=linked_blobs,
-                extras=extras)
-        return m
-
-    def check_spec(self, name):
-        """Check this measurement against a `Specification` level, of the
-        `Metric`.
-
-        Parameters
-        ----------
-        name : `str`
-            `Specification` level name.
-
-        Returns
-        -------
-        passed : `bool`
-            `True` if the measurement meets the `Specification` level, `False`
-            otherwise.
-
-        Notes
-        -----
-        Internally this method retrieves the `Specification` object, filtering
-        first by the ``name``, but also by this object's `filter_name`
-        attribute if specifications are filter-dependent.
-        """
-        return self.metric.check_spec(self.quantity, name,
-                                      filter_name=self.filter_name)
+        return cls(blobs=blobs, **json_data)
 
     def __eq__(self, other):
-        return (self.value == other.value) and (self.metric == other.metric)
+        return quantity_allclose(self.quantity, other.quantity) and \
+            (self.metric_name == other.metric_name)
 
     def __ne__(self, other):
         return not self.__eq__(other)
--- tests/test_measurements.py
+++ tests/test_measurements.py
@@ -4,10 +4,14 @@ from __future__ import print_function
 import unittest
 
 import astropy.units as u
+from astropy.tests.helper import quantity_allclose
 
-from lsst.verify import Measurement, MeasurementSet, Metric, MetricSet, Name
+from lsst.utils.tests import TestCase
+from lsst.verify import (Measurement, MeasurementSet, Metric, MetricSet, Name,
+                         Blob, Datum)
 
 
+@unittest.skip('DM-10199: skip while in development')
 class MeasurementSetTestCase(unittest.TestCase):
 
     def setUp(self):
@@ -43,49 +47,147 @@ class MeasurementSetTestCase(unittest.TestCase):
         self.assertEqual(str(ms), expect)
 
 
-class MeasurementTestCase(unittest.TestCase):
+class MeasurementTestCase(TestCase):
+    """Test lsst.verify.measurment.Measurement class."""
+
     def setUp(self):
-        # Need a metric set to check against
-        package = 'test'
-        self.package = package
-        metric1 = Metric("{}.radius".format(package), "some radius", u.arcsec)
-        metric2 = Metric("{}.cmodel_mag".format(package), "a magnitude", u.mag)
-        metric3 = Metric("{}.unitless".format(package), "no units!", '')
-        self.metric_set = MetricSet([metric1, metric2, metric3])
+        self.pa1 = Metric(
+            'validate_drp.PA1',
+            "The maximum rms of the unresolved source magnitude distribution "
+            "around the mean value (repeatability).",
+            'mmag',
+            tags=['photometric', 'LPM-17'],
+            reference_doc='LPM-17',
+            reference_url='http://ls.st/lpm-17',
+            reference_page=21)
 
-    def test_create_radius(self):
-        metric = 'test.radius'
-        value = 1235 * u.arcmin
-        measurement = Measurement(metric, value, metric_set=self.metric_set)
-        self.assertEqual(measurement.name, Name(metric))
-        self.assertEqual(measurement.value, value)
-        self.assertEqual(measurement.description, "some radius")
+        self.blob1 = Blob('Blob1')
+        self.blob1['datum1'] = Datum(5 * u.arcsec, 'Datum 1')
+        self.blob1['datum2'] = Datum(28. * u.mag, 'Datum 2')
 
-    def test_create_magnitude(self):
-        metric = 'test.cmodel_mag'
-        value = 1235 * u.mag
-        measurement = Measurement(metric, value, metric_set=self.metric_set)
-        self.assertEqual(measurement.name, Name(metric))
-        self.assertEqual(measurement.value, value)
-        self.assertEqual(measurement.description, "a magnitude")
+        self.blob2 = Blob('Blob2')
+        self.blob2['datumN'] = Datum(11 * u.dimensionless_unscaled, 'Count')
 
-    def test_create_invalid_name(self):
-        metric = 'test.not_valid'
-        value = 1235 * u.arcsec
-        with self.assertRaises(KeyError):
-            Measurement(metric, value, metric_set=self.metric_set)
+    def test_PA1_measurement_with_metric(self):
+        """Standard metric with a given Metric instance."""
+        measurement = Measurement(self.pa1, 0.002 * u.mag, blobs=[self.blob1])
+        measurement.link_blob(self.blob2)
 
-    @unittest.skip('FIXME DM-8477 Requires Astropy 1.3+')
-    def test_create_invalid_unit(self):
-        metric = 'test.unitless'
-        value = 1235 * u.arcsec
-        with self.assertRaises(u.UnitTypeError):
-            Measurement(metric, value, metric_set=self.metric_set)
+        measurement2 = Measurement(self.pa1, 0.002 * u.mag)
+
+        self.assertTrue(quantity_allclose(measurement.quantity, 0.002 * u.mag))
+        self.assertIsInstance(measurement.metric_name, Name)
+        self.assertEqual(measurement.metric_name, Name('validate_drp.PA1'))
+        self.assertEqual(measurement.metric, self.pa1)
+        self.assertNotEqual(measurement.identifier, measurement2.identifier)
+
+        # Test blob access
+        self.assertIn('Blob1', measurement.blobs)
+        self.assertIn('Blob2', measurement.blobs)
+
+        # Test Datum representation
+        datum = measurement.datum
+        self.assertTrue(quantity_allclose(datum.quantity, 0.002 * u.mag))
+        self.assertEqual(datum.label, str(self.pa1.name))
+        self.assertEqual(datum.description, str(self.pa1.description))
+
+        # Test serialization
+        json_doc = measurement.json
+        # Units should be cast to those of the metric
+        self.assertEqual(json_doc['unit'], 'mmag')
+        self.assertFloatsAlmostEqual(json_doc['value'], 2.0)
+        self.assertEqual(json_doc['identifier'], measurement.identifier)
+        self.assertIsInstance(json_doc['blob_refs'], list)
+        self.assertIn(self.blob1.identifier, json_doc['blob_refs'])
+        self.assertIn(self.blob2.identifier, json_doc['blob_refs'])
+        # No extras, so should not be serialized
+        self.assertNotIn(measurement.extras.identifier, json_doc['blob_refs'])
+
+        # Test deserialization
+        new_measurement = Measurement.deserialize(
+            blobs=[self.blob1, self.blob2],
+            **json_doc)
+        self.assertEqual(measurement, new_measurement)
+        self.assertIn('Blob1', measurement.blobs)
+        self.assertIn('Blob2', measurement.blobs)
+
+    def test_PA1_measurement_without_metric(self):
+        """Test a measurement without a Metric instance."""
+        measurement = Measurement('validate_drp.PA1', 0.002 * u.mag)
+
+        self.assertIsInstance(measurement.metric_name, Name)
+        self.assertEqual(measurement.metric_name, Name('validate_drp.PA1'))
+        self.assertIsNone(measurement.metric)
+
+        json_doc = measurement.json
+        # Units are not converted
+        self.assertEqual(json_doc['unit'], 'mag')
+        self.assertFloatsAlmostEqual(json_doc['value'], 0.002)
+
+        new_measurement = Measurement.deserialize(**json_doc)
+        self.assertEqual(measurement, new_measurement)
+
+    def test_PA1_deferred_metric(self):
+        """Test a measurement when the Metric instance is added later."""
+        measurement = Measurement('PA1', 0.002 * u.mag)
+
+        self.assertIsNone(measurement.metric)
+        self.assertEqual(measurement.metric_name, Name(metric='PA1'))
+
+        # Try adding in a metric with the wrong units to existing quantity
+        other_metric = Metric('testing.other', 'Incompatible units', 'arcsec')
+        with self.assertRaises(TypeError):
+            measurement.metric = other_metric
+
+        # Add metric in; the name should also update
+        measurement.metric = self.pa1
+        self.assertEqual(measurement.metric, self.pa1)
+        self.assertEqual(measurement.metric_name, self.pa1.name)
+
+    def test_PA1_deferred_quantity(self):
+        """Test a measurement where the quantity is added later."""
+        measurement = Measurement(self.pa1)
+        json_doc = measurement.json
+        self.assertIsNone(json_doc['unit'])
+        self.assertIsNone(json_doc['value'])
+
+        with self.assertRaises(TypeError):
+            # wrong units
+            measurement.quantity = 5 * u.arcsec
+
+        measurement.quantity = 5 * u.mmag
+        quantity_allclose(measurement.quantity, 5 * u.mmag)
+
+    def test_creation_with_extras(self):
+        """Test creating a measurement with an extra."""
+        measurement = Measurement(
+            self.pa1, 5. * u.mmag,
+            extras={'extra1': Datum(10. * u.arcmin, 'Extra 1')})
+
+        self.assertIn(str(self.pa1.name), measurement.blobs)
+        self.assertIn('extra1', measurement.extras)
+
+        json_doc = measurement.json
+        self.assertIn(measurement.extras.identifier, json_doc['blob_refs'])
+
+        blobs = [b for k, b in measurement.blobs.items()]
+        new_measurement = Measurement.deserialize(blobs=blobs, **json_doc)
+        self.assertIn('extra1', new_measurement.extras)
+        self.assertEqual(measurement, new_measurement)
+
+    def test_deferred_extras(self):
+        """Test adding extras to an existing measurement."""
+        measurement = Measurement(self.pa1, 5. * u.mmag)
+
+        self.assertIn(str(self.pa1.name), measurement.blobs)
+
+        measurement.extras['extra1'] = Datum(10. * u.arcmin, 'Extra 1')
+        self.assertIn('extra1', measurement.extras)
 
     def test_str(self):
         metric = 'test.cmodel_mag'
         value = 1235 * u.mag
-        m = Measurement(metric, value, metric_set=self.metric_set)
+        m = Measurement(metric, value)
         self.assertEqual(str(m), "test.cmodel_mag: 1235.0 mag")
 
 
