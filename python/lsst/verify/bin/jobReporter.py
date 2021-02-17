865a4f125edd5db3f862e708eddc2c586091b629
Krzysztof Findeisen <kfindeis@uw.edu>
2021-02-17 17:52:27 -0600

    Fix flake8 errors in jobReporter.py.
    The metric-pipeline-tasks/faro repository used a 110-character limit;
    verify uses 80.

--- python/lsst/verify/bin/jobReporter.py
+++ python/lsst/verify/bin/jobReporter.py
@@ -3,11 +3,17 @@ from lsst.daf.butler import Butler
 
 
 class JobReporter:
-    def __init__(self, repository, collection, metrics_package, spec, dataset_name):
+    def __init__(self,
+                 repository,
+                 collection,
+                 metrics_package,
+                 spec,
+                 dataset_name):
         # Hard coding verify_metrics as the packager for now.
         # It would be easy to pass this in as an argument, if necessary.
-        self.metrics = MetricSet.load_metrics_package(package_name_or_path='verify_metrics',
-                                                      subset=metrics_package)
+        self.metrics = MetricSet.load_metrics_package(
+            package_name_or_path='verify_metrics',
+            subset=metrics_package)
         self.butler = Butler(repository)
         self.registry = self.butler.registry
         self.spec = spec
@@ -17,18 +23,20 @@ class JobReporter:
     def run(self):
         jobs = {}
         for metric in self.metrics:
-            data_ids = list(self.registry.queryDatasets((f'metricvalue_{metric.package}'
-                                                         f'_{metric.metric}'),
+            dataset = f'metricvalue_{metric.package}_{metric.metric}'
+            data_ids = list(self.registry.queryDatasets(dataset,
                             collections=self.collection))
             for did in data_ids:
                 m = self.butler.get(did, collections=self.collection)
                 # make the name the same as what SQuaSH Expects
                 m.metric_name = metric
                 # Grab the physical filter associated with the abstract filter
-                # In general there may be more than one.  Take the shortest assuming
-                # it is the most generic.
-                pfilts = [el.name for el in self.butler.registry.queryDimensionRecords('physical_filter',
-                                                                                       dataId=did.dataId)]
+                # In general there may be more than one.  Take the shortest
+                # assuming it is the most generic.
+                pfilts = [el.name for el in
+                          self.registry.queryDimensionRecords(
+                              'physical_filter',
+                              dataId=did.dataId)]
                 pfilt = min(pfilts, key=len)
 
                 tract = did.dataId['tract']
